b7df40a92ed7187655d974da36f6dadbd914ef8a
Joshua Lam <jlam26.ca@gmail.com>
2020-04-17 02:27:15 -0700

    Link Adapter to Firestore

--- app/src/main/java/ca/bcit/comp3717_final_exam/MainActivity.java
+++ app/src/main/java/ca/bcit/comp3717_final_exam/MainActivity.java
@@ -1,17 +1,22 @@
 package ca.bcit.comp3717_final_exam;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.firestore.CollectionReference;
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
-import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.firestore.Query;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -20,6 +25,9 @@ public class MainActivity extends AppCompatActivity {
 
     FirebaseFirestore db;
     EditText title, description, url;
+    RecyclerView mFirestoreList;
+
+    private FirestoreRecyclerAdapter adapter;
 
 
     @Override
@@ -32,6 +40,7 @@ public class MainActivity extends AppCompatActivity {
         description = findViewById(R.id.description);
         url = findViewById(R.id.url);
         Button button = findViewById(R.id.button);
+        mFirestoreList = findViewById(R.id.movies);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -42,19 +51,31 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        readSingleMovieCustomObject();
-    }
+        Query query = db.collection("MovieItem");
 
-    private void readSingleMovieCustomObject() {
-        CollectionReference movies = db.collection("MovieItem");
-        movies.get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+        FirestoreRecyclerOptions<MovieItem> options = new FirestoreRecyclerOptions.Builder<MovieItem>()
+                .setQuery(query, MovieItem.class)
+                .build();
+
+        adapter = new FirestoreRecyclerAdapter<MovieItem, MoviesViewHolder>(options) {
+            @NonNull
             @Override
-            public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
-                for (QueryDocumentSnapshot doc : queryDocumentSnapshots) {
-                    MovieItem readMovie = doc.toObject(MovieItem.class);
-                }
+            public MoviesViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item_single, parent, false);
+                return new MoviesViewHolder(view);
             }
-        });
+
+            @Override
+            protected void onBindViewHolder(@NonNull MoviesViewHolder holder, int position, @NonNull MovieItem model) {
+                holder.list_title.setText(model.getTitle());
+                holder.list_description.setText(model.getDescription());
+                holder.list_url.setText(model.getUrl());
+            }
+        };
+
+        mFirestoreList.setHasFixedSize(true);
+        mFirestoreList.setLayoutManager(new LinearLayoutManager(this));
+        mFirestoreList.setAdapter(adapter);
     }
 
 
@@ -65,5 +86,32 @@ public class MainActivity extends AppCompatActivity {
         newMovie.put("url", url);
 
         db.collection("MovieItem").document(title).set(newMovie);
+    }
+
+    private class MoviesViewHolder extends RecyclerView.ViewHolder{
+
+        private TextView list_title;
+        private TextView list_description;
+        private TextView list_url;
+
+        public MoviesViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            list_title = itemView.findViewById(R.id.list_title);
+            list_description = itemView.findViewById(R.id.list_description);
+            list_url = itemView.findViewById(R.id.list_url);
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        adapter.stopListening();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        adapter.startListening();
     }
 }
